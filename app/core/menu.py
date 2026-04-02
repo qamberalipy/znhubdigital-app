21b5712bacb244ffb63afefc03517322714cee66
Qamber ali <qamber246@gmail.com>
2026-04-02 17:08:08 +0500

    Ui Changes

--- app/core/menu.py
+++ app/core/menu.py
@@ -7,7 +7,6 @@ MENU = {
         {"label": "Task Assigner", "icon": "ri-task-line", "path": "/task_assigner", "children": []},
         {"label": "Projects", "icon": "ri-folder-line", "path": "/projects", "children": []},
         {"label": "Signature Assigner", "icon": "ri-file-text-line", "path": "/signature_assigner", "children": []},
-        {"label": "Content Vault", "icon": "ri-folder-line", "path": "/content_vault", "children": []},
         {"label": "Announcements", "icon": "ri-discuss-line", "path": "/admin_feed", "children": []},
         {"label": "Model Invoices", "icon": "ri-file-check-fill", "path": "/model_invoices", "children": []},
         {"label": "Invoice Reports", "icon": "ri-file-chart-line", "path": "/model_invoices/report", "children": []},
@@ -17,7 +16,6 @@ MENU = {
         {"label": "View Leads", "icon": "ri-eye-line", "path": "/view-lead", "children": []},
         {"label": "Task Assigner", "icon": "ri-task-line", "path": "/task_assigner", "children": []},
         {"label": "Signature Assigner", "icon": "ri-file-text-line", "path": "/signature_assigner", "children": []},
-        {"label": "Content Vault", "icon": "ri-folder-line", "path": "/content_vault", "children": []},
         {"label": "Announcements", "icon": "ri-discuss-line", "path": "/admin_feed", "children": []},
     ],
     "sale": [
@@ -29,7 +27,7 @@ MENU = {
     ],
     "client": [
         {"label": "Dashboard", "icon": "ri-home-4-line", "path": "/dashboard", "children": []},
-        {"label": "Task Submission", "icon": "ri-file-upload-line", "path": "/task_submission", "children": []},
+        {"label": "Task Submission", "icon": "ri-file-upload-line", "path": "/task_assigner", "children": []},
         {"label": "Signature Signer", "icon": "ri-file-text-line", "path": "/signature_signer", "children": []},
         {"label": "Announcements", "icon": "ri-discuss-line", "path": "/admin_feed", "children": []},
     ],
